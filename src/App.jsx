ab1a0b00dd43f202fd6a6fca93434cb58fdc1bed
Ajith101 <ajithpktd@gmail.com>
2023-04-19 15:44:24 +0530

    MERN Task

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const onIncrement = () => {
+    if (count !== 10) {
+      setCount((pre) => pre + 1);
+    }
+  };
+  const onDecrement = () => {
+    if (count !== 0) {
+      setCount((pre) => pre - 1);
+    }
+  };
+  return (
+    <div className="App">
+      <Counter
+        count={count}
+        onIncrement={onIncrement}
+        onDecrement={onDecrement}
+      />
+    </div>
+  );
+}
+
+export default App;
