fc7073cde341a1556d022b6d48cf6b5479d13310
Ajith101 <ajithpktd@gmail.com>
2023-05-01 18:12:53 +0530

    small changes

--- src/components/addTodo/AddTodo.jsx
+++ src/components/addTodo/AddTodo.jsx
@@ -11,13 +11,18 @@ export const AddTodo = ({
     <>
       <div className="add-todo-container">
         <form className="input-section" onSubmit={submitHandler}>
-          <input
-            value={todoText.todo}
-            type="text"
-            name="todo"
-            onChange={inputHandler}
-          />
-          <button type="submit">ADD TODO</button>
+          <div className="input-box">
+            <input
+              value={todoText.todo}
+              type="text"
+              name="todo"
+              onChange={inputHandler}
+            />
+          </div>
+
+          <div className="">
+            <button type="submit">ADD TODO</button>
+          </div>
         </form>
         {errorValues?.todo && (
           <p style={{ color: "red", margin: "5px 0", textAlign: "left" }}>
